d2d7ea158abb08ec7be9421616b69243813b4fe0
roman shushakov <roman.a.shushakov@mail.ru>
2021-02-08 15:34:11 +0300

    rotation matrix draft was added

--- src/main.rs
+++ src/main.rs
@@ -77,4 +77,60 @@ fn main()
             unit_matrix.show_matrix();
         }
     }
+
+
+    let x = 0.0;
+    let y = -1.0;
+    let z = 1.0;
+    let length = f64::sqrt(3. * 3. + 3. * 3. + 3. * 3.);
+    let alpha = f64::acos(3. / length).to_degrees();
+
+    let heading = f64::atan2(
+        y * alpha.to_radians().sin() - x * z * (1.0 - alpha.to_radians().cos()),
+        1.0 - (y * y + z * z ) * (1.0 - alpha.to_radians().cos()));
+    println!("Heading: {}", heading.to_degrees());
+
+    let attitude = f64::asin(
+        x * y * (1.0 - alpha.to_radians().cos()) + z * alpha.to_radians().sin());
+    println!("Attitude: {}", attitude.to_degrees());
+
+    let bank = f64::atan2(
+        x * alpha.to_radians().sin() - y * z * (1.0 - alpha.to_radians().cos()) ,
+        1.0 - (x * x + z * z) * (1.0 - alpha.to_radians().cos()));
+    println!("Bank: {}", bank.to_degrees());
+
+    let q_11 = heading.cos() * attitude.cos();
+    println!("Q_11: {}", q_11);
+    let q_12 = - heading.cos() * attitude.sin() * bank.cos() + heading.sin() * bank.sin();
+    println!("Q_12: {}", q_12);
+    let q_13 = heading.cos() * attitude.sin() * bank.sin() + heading.sin() * bank.cos();
+    println!("Q_13: {}", q_13);
+    let q_21 = attitude.sin();
+    println!("Q_21: {}", q_21);
+    let q_22 = attitude.cos() * bank.cos();
+    println!("Q_22: {}", q_22);
+    let q_23 = - attitude.cos() * bank.sin();
+    println!("Q_23: {}", q_23);
+    let q_31 = - heading.sin() * attitude.cos();
+    println!("Q_31: {}", q_31);
+    let q_32 = heading.sin() * attitude.sin() * bank.cos() + heading.cos() * bank.sin();
+    println!("Q_32: {}", q_32);
+    let q_33 = - heading.sin() * attitude.sin() * bank.sin() + heading.cos() * bank.cos();
+    println!("Q_33: {}", q_33);
+
+    let v_1 = ExtendedMatrix::create(
+        3u16, 1u16, vec![3., 3., 3.]);
+    let v_2 = ExtendedMatrix::create(
+        3u16, 1u16, vec![length, 0., 0.]);
+    let rotational_matrix = ExtendedMatrix::create(
+        3u16, 3u16,
+        vec![q_11, q_12, q_13, q_21, q_22, q_23, q_31, q_32, q_33]);
+    if let Ok(inv_m) = rotational_matrix.inverse()
+    {
+        if let Ok(m) = inv_m.multiply_by_matrix(&v_1)
+        {
+            m.show_matrix();
+        }
+    }
+
 }
