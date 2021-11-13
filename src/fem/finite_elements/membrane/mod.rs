f1d2babf59a1871913b8b6d074b72159c38a1b0c
roman shushakov <roman.a.shushakov1@gmail.com>
2021-11-13 21:59:38 +0300

    More changes

--- src/fem/finite_elements/membrane/mod.rs
+++ src/fem/finite_elements/membrane/mod.rs
@@ -1,2 +1,5 @@
+pub mod mem4n4ip;
 pub(super) mod consts;
-pub (super) mod quad_mem_aux_functions;
+// pub (super) mod quad_mem_aux_functions;
+pub mod quad_mem_aux_functions;
+
--- src/fem/finite_elements/mod.rs
+++ src/fem/finite_elements/mod.rs
@@ -1,6 +1,8 @@
 pub mod fe_node;
 pub(super) mod truss;
-pub(super) mod functions;
+// pub(super) mod functions;
+pub mod functions;
 pub mod finite_element;
 pub(super) mod beam;
-pub(super) mod membrane;
+// pub(super) mod membrane;
+pub mod membrane;
