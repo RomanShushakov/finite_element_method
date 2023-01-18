e0fcfcc9c63d9933b142636c5e4c291d0e401992
roman shushakov <roman.a.shushakov1@gmail.com>
2023-01-18 14:50:43 +0500

    Errors in convex_hull_on_planes mod have been fixed

--- src/fem/convex_hull_on_plane.rs
+++ src/fem/convex_hull_on_plane.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 use std::cmp::Ordering;
 
-use extended_matrix::traits::{UIntTrait, FloatTrait};
+use extended_matrix::FloatTrait;
 
 
 pub fn quick_sort<T>(arr: &mut [T])
@@ -27,19 +27,19 @@ fn _quick_sort<T>(arr: &mut [T], low: isize, high: isize)
 fn partition<T>(arr: &mut [T], low: isize, high: isize) -> isize
     where T: PartialOrd
 {
-    let pivot = high as usize;
+    let pivot = high as u32;
     let mut store_index = low - 1;
     let mut last_index = high;
 
     loop
     {
         store_index += 1;
-        while arr[store_index as usize] < arr[pivot]
+        while arr[store_index as u32] < arr[pivot]
         {
             store_index += 1;
         }
         last_index -= 1;
-        while last_index >= 0 && arr[last_index as usize] > arr[pivot]
+        while last_index >= 0 && arr[last_index as u32] > arr[pivot]
         {
             last_index -= 1;
         }
@@ -49,131 +49,137 @@ fn partition<T>(arr: &mut [T], low: isize, high: isize) -> isize
         }
         else
         {
-            arr.swap(store_index as usize, last_index as usize);
+            arr.swap(store_index as u32, last_index as u32);
         }
     }
-    arr.swap(store_index as usize, pivot as usize);
+    arr.swap(store_index as u32, pivot as u32);
     store_index
 }
 
 
 #[derive(Debug, Clone)]
-pub struct Point<T, V>
+pub struct Point<V>
 {
-    n: V,
-    x: T,
-    y: T,
+    n: u32,
+    x: V,
+    y: V,
 }
 
 
-impl<T, V> Point<T, V>
-    where V: UIntTrait<Output = V>,
-          T: FloatTrait<Output = T, Other = T>
+impl<V> Point<V>
+    where V: FloatTrait<Output = V, Other = V>,
 {
-    pub fn create(n: V, x: T, y: T) -> Self
+    pub fn create(n: u32, x: V, y: V) -> Self
     {
         Point { n, x, y }
     }
 
 
-    fn vector_from_origin_to_point(&self) -> Vector<T, V>
+    fn vector_from_origin_to_point(&self) -> Vector<V>
     {
-        Vector::create(
-            V::from(0u8), T::from(0.0), T::from(0.0),
-            self.n, self.x, self.y
-        )
+        Vector::create(0, V::from(0.0), V::from(0.0), self.n, self.x, self.y)
     }
 
 
-    pub fn copy_number(&self) -> V
+    pub fn copy_number(&self) -> u32
     {
         self.n
     }
 }
 
 
-impl<T, V> PartialOrd for Point<T, V>
-    where V: UIntTrait<Output = V>,
-          T: FloatTrait<Output = T, Other = T>
+impl<V> PartialOrd for Point<V>
+    where V: FloatTrait<Output = V, Other = V>
 {
-    fn partial_cmp(&self, other: &Point<T, V>) -> Option<Ordering>
-    {
-        let directional_vector: Vector<T, V> = Vector::create_directional_vector();
-        let lhs_vector_from_origin = self.vector_from_origin_to_point();
-        let rhs_vector_from_origin = other.vector_from_origin_to_point();
-        directional_vector.cosine_of_angle_between_vectors(&lhs_vector_from_origin).my_acos().my_to_degrees()
-            .partial_cmp(&(directional_vector.cosine_of_angle_between_vectors(&rhs_vector_from_origin).my_acos())
-                .my_to_degrees())
-    }
-}
-
-
-impl<T, V> PartialEq for Point<T, V>
-    where V: UIntTrait<Output = V>,
-          T: FloatTrait<Output = T, Other = T>
-{
-    fn eq(&self, other: &Point<T, V>) -> bool
+    fn partial_cmp(&self, other: &Point<V>) -> Option<Ordering>
     {
         let directional_vector = Vector::create_directional_vector();
         let lhs_vector_from_origin = self.vector_from_origin_to_point();
         let rhs_vector_from_origin = other.vector_from_origin_to_point();
-        directional_vector.cosine_of_angle_between_vectors(&lhs_vector_from_origin).my_acos().my_to_degrees() ==
-            directional_vector.cosine_of_angle_between_vectors(&rhs_vector_from_origin).my_acos().my_to_degrees()
+        directional_vector
+            .cosine_of_angle_between_vectors(&lhs_vector_from_origin)
+            .my_acos()
+            .my_to_degrees()
+            .partial_cmp(
+                &(directional_vector
+                    .cosine_of_angle_between_vectors(&rhs_vector_from_origin)
+                    .my_acos()
+                )
+                .my_to_degrees()
+            )
+    }
+}
+
+
+impl<V> PartialEq for Point<V>
+    where V: FloatTrait<Output = V, Other = V>
+{
+    fn eq(&self, other: &Point<V>) -> bool
+    {
+        let directional_vector = Vector::create_directional_vector();
+        let lhs_vector_from_origin = self.vector_from_origin_to_point();
+        let rhs_vector_from_origin = other.vector_from_origin_to_point();
+        directional_vector
+            .cosine_of_angle_between_vectors(&lhs_vector_from_origin)
+            .my_acos()
+            .my_to_degrees() ==
+        directional_vector
+            .cosine_of_angle_between_vectors(&rhs_vector_from_origin)
+            .my_acos()
+            .my_to_degrees()
 
     }
 }
 
 
 #[derive(Debug)]
-struct Vector<T, V>
+struct Vector<V>
 {
-    point_1: Point<T, V>,
-    point_2: Point<T, V>,
+    point_1: Point<V>,
+    point_2: Point<V>,
 }
 
 
-fn double_signed_area<T, V>(p_1: &Point<T, V>, p_2: &Point<T, V>, p_3: &Point<T, V>) -> T
-    where T: FloatTrait<Output = T, Other = T>
+fn double_signed_area<V>(p_1: &Point<V>, p_2: &Point<V>, p_3: &Point<V>) -> V
+    where V: FloatTrait<Output = V, Other = V>
 {
     (p_2.x - p_1.x) * (p_3.y - p_1.y) - (p_2.y - p_1.y) * (p_3.x - p_1.x)
 }
 
 
-impl<T, V> Vector<T, V>
-    where V: UIntTrait<Output = V>,
-          T: FloatTrait<Output = T, Other = T>
+impl<V> Vector<V>
+    where V: FloatTrait<Output = V, Other = V>
 {
     fn create_directional_vector() -> Self
     {
-        Vector::create(
-            V::from(0u8), T::from(0.0), T::from(0.0),
-            V::from(0u8), T::from(1.0), T::from(0.0)
-        )
+        Vector::create(0, V::from(0.0), V::from(0.0), 0, V::from(1.0), V::from(0.0))
     }
 
 
-    fn create(n_1: V, x_1: T, y_1: T, n_2: V, x_2: T, y_2: T) -> Self
+    fn create(n_1: u32, x_1: V, y_1: V, n_2: u32, x_2: V, y_2: V) -> Self
     {
         let point_1 = Point { n: n_1, x: x_1, y: y_1 };
         let point_2 = Point { n: n_2, x: x_2, y: y_2 };
         Vector { point_1, point_2 }
     }
 
-    fn vector_length(&self) -> T
+    fn vector_length(&self) -> V
     {
-        ((self.point_1.x - self.point_2.x) * (self.point_1.x - self.point_2.x) +
-            (self.point_1.y - self.point_2.y) * (self.point_1.y - self.point_2.y)).my_sqrt()
+        (
+            (self.point_1.x - self.point_2.x) * (self.point_1.x - self.point_2.x) +
+            (self.point_1.y - self.point_2.y) * (self.point_1.y - self.point_2.y)
+        ).my_sqrt()
     }
 
 
-    fn scalar_product(&self, other: &Vector<T, V>) -> T
+    fn scalar_product(&self, other: &Vector<V>) -> V
     {
         (self.point_1.x - self.point_2.x) * (other.point_1.x - other.point_2.x) +
         (self.point_1.y - self.point_2.y) * (other.point_1.y - other.point_2.y)
     }
 
 
-    fn cosine_of_angle_between_vectors(&self, other: &Vector<T, V>) -> T
+    fn cosine_of_angle_between_vectors(&self, other: &Vector<V>) -> V
     {
         let lhs_length = self.vector_length();
         let rhs_length = other.vector_length();
@@ -183,9 +189,8 @@ impl<T, V> Vector<T, V>
 }
 
 
-pub fn convex_hull_on_plane<T, V>(data: &[Point<T, V>]) -> Vec<Point<T, V>>
-    where V: UIntTrait<Output = V>,
-          T: FloatTrait<Output = T, Other = T>
+pub fn convex_hull_on_plane<V>(data: &[Point<V>]) -> Vec<Point<V>>
+    where V: FloatTrait<Output = V, Other = V>,
 {
     let mut updated_data = data.to_vec();
     let mut shift_x = updated_data[0].x;
@@ -212,7 +217,7 @@ pub fn convex_hull_on_plane<T, V>(data: &[Point<T, V>]) -> Vec<Point<T, V>>
     {
         let doubled_area = double_signed_area(
             &updated_data[i], &updated_data[i + 1], &updated_data[i + 2]);
-        if doubled_area <= T::from(0.0)
+        if doubled_area <= V::from(0.0)
         {
             updated_data.remove(i + 1);
         }
